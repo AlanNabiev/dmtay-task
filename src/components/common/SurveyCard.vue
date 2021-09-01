52cd1b0aca2907e8340877a783f8f8ffb28b1404
AlanNabiev <alannabiev2001@gmail.com>
2021-09-01 23:09:18 +0300

    feat: common components

---
+++ src/components/common/SurveyCard.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="mt-20 space-y-8">
+    <span class="mt-10 text-xl text-green-700 font-bold">{{ question }}</span>
+    <BaseRadioGroup @selectAnswer="selectedAnswer" :options="answers" />
+    <BaseButton color="green">Select</BaseButton>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  name: "SurveyCard",
+  props: {
+    question: String,
+    answers: {
+      type: Array,
+      default: function () {
+        return ["Startup", "Business", "Enterprise"];
+      }
+    }
+  },
+  setup() {
+    const answer = ref(null);
+
+    function selectedAnswer(selected) {
+      answer.value = selected;
+    }
+
+    return {
+      answer,
+      selectedAnswer
+    };
+  }
+});
+</script>
